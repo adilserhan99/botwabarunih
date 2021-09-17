0790d8f987c1f47f44749c8c1ac63fa081ea9186
adilserhan99 <90885620+adilserhan99@users.noreply.github.com>
2021-09-18 06:05:08 +0700

    Add files via upload

---
+++ language/nad.js
@@ -0,0 +1,156 @@
+exports.wait = () => {
+	return`*「❗」WAIT KAK*`
+}
+
+exports.succes = () => {
+	return`*「 SUKSES 」*`
+}
+
+exports.lvlon = () => {
+	return`*「❗」LEVELING ENABLE*`
+}
+
+exports.lvloff = () => {
+	return`*「❗」LEVELING DISABLE*`
+}
+
+exports.lvlnul = () => {
+	return`*「❗」LEVELMU MASIH KOSONG*`
+}
+
+exports.lvlnoon = () => {
+	return`*「❗」LEVEL DI GRUB BELUM DI AKTIFKAN*`
+}
+
+exports.nsfwoff = () => {
+	return`*「❗」NSFW BELUM DI AKTIFKAN*`
+}
+
+exports.noregis = () => {
+	return`*「❗」BELUM VERIFIKASI「❗」*\n\nketik : @verify`
+}
+
+exports.baned = () => {
+	return`*「❗」SORRY SORRY AJA NIH BRO, TAPI KAU SUDAH KU BANNED YAHAHAHA HAYUUU :V*`
+}
+
+exports.premium = (prefix) => {
+	return`Lu Siapa? Fitur ini khusus user premium!
+Upgrade ke premium dulu bosku
+Jika minat hubungi owner ku
+Ketik : ${prefix}owner`
+}
+
+exports.rediregis = () => {
+	return`*Kamu sudah melakukan verify -_-*`
+}
+
+exports.stikga = () => {
+	return`*「 GAGAL 」Coba reply/tag ulang kak*`
+}
+
+exports.linkga = () => {
+	return`*「❗」maaf linknya tidak valid kak*`
+}
+
+exports.groupo = () => {
+	return`*「❗」GROUP ONLY TOD*`
+}
+
+exports.ownerb = () => {
+	return`*「❗」OWNER BOT ONLY TOD*`
+}
+
+exports.ownerg = () => {
+	return`*「❗」OWNER GROUP ONLY TOD*`
+}
+
+exports.admin = () => {
+	return`*「❗」ADMIN GROUP ONLY TOD*`
+}
+
+exports.badmin = () => {
+	return`*「❗」BOT HARUS JADI ADMIN TOD*`
+}
+
+exports.bug = () => {
+	return`*Masalah telah di laporkan ke owner BOT, laporan palsu/main2 tidak akan ditanggapi*`
+}
+
+exports.wrongf = () => {
+	return`*「🗿」Teks nya mana Tod?*`
+}
+
+exports.clears = () => {
+	return`*Sukses bosku*`
+}
+
+exports.levelup = (pushname, sender, getLevelingXp,  getLevel, getLevelingLevel) => {
+	return`
+*「 SELAMAT 」*
+\`\`\`➸ Nama : ${pushname}\`\`\`
+\`\`\`➸ Nomor : wa.me/${sender.split("@")[0]}\`\`\`
+\`\`\`➸ Xp : ${getLevelingXp(sender)}\`\`\`
+\`\`\`➸ Level : ${getLevel} ➸ ${getLevelingLevel(sender)}\`\`\`
+`}
+ 
+exports.limitend = (pushname, prefix) => {
+	return`*maaf ${pushname} limit hari ini habis*
+*Limit di reset setiap jam 24:00*
+
+\`\`\`Upgrade Premium Bebas Limit\`\`\`
+\`\`\`➸ 20K : 1 Bulan\`\`\`
+\`\`\`Jika berminat silahkan hubungi owner\`\`\`
+\`\`\`Kirim perintah : ${prefix}owner\`\`\`
+`
+}
+
+exports.limitcount = (isPrem, limitCounts) => {
+	return`
+*「 LIMIT COUNT 」*
+sisa limit anda : ${isPrem ? '9999' : `${limitCounts}`}
+
+Upgrade premium bosku, biar bebas gunain bot`
+}
+
+exports.uangkau = (pushname, sender, uangkau) => {
+	return`┏━━━━━━━♡ *ATM* ♡━━━━━━━┓
+┃╭───────────────────
+┃│➸ NAMA : ${pushname}
+┃│➸ NOMOR : ${sender.split("@")[0]}
+┃│➸ UANG : ${uangkau}
+┃╰───────────────────
+┗━━━━━━━━━━━━━━━━━━━━┛`
+}
+
+exports.donasi = () => {
+return`*DONASI KAK*
+Pulsa : 083153406515
+Gopay : Minta Owner
+
+Donasi supaya bot terus update & Aktif`
+}
+
+exports.iklan = (botName, ownerNumbers, ownerName) => {
+return`🔰 -----[ 「 *SEWA ${botName}* 」 ]----- 🔰
+──────────────────────────────
+◯ *DAFTAR SEWA & BUAT BOT :*
+◯ *SEWA : 5K/GRUP (SEMINGGU)*
+◯ *SEWA : 10K/GRUP (BULAN)*
+◯ *SEWA : 15K/GRUP (PERMANEN)*
+◯ *PEMBAYARAN BISA MELALUI :*
+◯ *GOPAY, DANA*
+──────────────────────────────
+◯ *KEUNTUNGAN SEWA BOT :*
+◯ *1. BISA MEMASUKAN BOT KE GROUP*
+◯ *2. BISA MENGGUNAKAN FITUR PREMIUM*
+◯ *3. BISA BUAT JAGA GRUP*
+◯ *4. BISA BUAT MAINAN*
+◯ *5. BISA BUAT BERSENANG SENANG*
+──────────────────────────────
+◯ *JIKA MINAT IKLAN DIATAS*
+◯ *HARAP HUBUNGI NOMOR DIBAWAH :*
+◯ *wa.me/6283153406515*
+──────────────────────────────
+🔰 -----[「 *POWERED BY RIYAN BOTZ* 」]----- 🔰`
+}
